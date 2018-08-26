9a5c5aa0817a4f87145af259e1e82ce539a18916
Nicholas Meyer <nicmeyer@protonmail.com>
2018-08-26 14:30:11 +0200

    removed decorater code that is unnecessary for this example

--- logit.py
+++ logit.py
@@ -3,16 +3,6 @@ import tensorflow as tf
 from functools import wraps
 from tensorflow.examples.tutorials.mnist import input_data
 
-def doublewrap(function):
-    @wraps(function)
-    def decorator(*args, **kwargs):
-        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
-            return function(args[0])
-        else:
-            return lambda wrapee: function(wrapee, *args, **kwargs)
-    return decorator
-
-@doublewrap
 def define_scope(function, scope=None, *args, **kwargs):
     attribute = '_cache_' + function.__name__
     name = scope or function.__name__
